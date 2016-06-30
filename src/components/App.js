34b04c5a5aa79a624d091bc8f787d997d973f0c4
Kenneth Lee <kennyxlee@gmail.com>
2016-06-30 16:51:01 -0700

    Create InputForm component and connect it to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 
+import InputForm from './InputForm';
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      height: '',
+      weight: '',
+    };
+
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleFormSubmit(height, weight) {
+    this.setState({
+      height,
+      weight,
+    });
+
+    console.log('Height is', this.state.height);
+    console.log('Weight is', this.state.weight);
+  }
+
   render() {
     return (<div>
       <h3>BMI Calculator</h3>
+      <InputForm handleFormSubmit={this.handleFormSubmit} />
     </div>);
   }
 }
---
+++ src/components/InputForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+export default class InputForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      height: '',
+      weight: '',
+    }
+
+    this.onHeightChange = this.onHeightChange.bind(this);
+    this.onWeightChange = this.onWeightChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onHeightChange(event) {
+    this.setState({ height: event.target.value });
+  }
+
+  onWeightChange(event) {
+    this.setState({ weight: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    let { height, weight } = this.state;
+
+    if (height && weight) {
+      this.props.handleFormSubmit(height, weight);
+
+      this.setState({
+        height: '',
+        weight: '',
+      })
+    }
+  }
+
+  render() {
+    return (<form onSubmit={this.onFormSubmit}>
+      <input
+        placeholder="Your height (inches)"
+        onChange={this.onHeightChange}
+      />
+      <input
+        placeholder="Your Weight (pounds)"
+        onChange={this.onWeightChange}
+      />
+      <button type="submit">Submit</button>
+    </form>);
+  }
+}
