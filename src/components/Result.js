50e83ecf0e65c703dba818eaa7e0ad42117d54a3
Kenneth Lee <kennyxlee@gmail.com>
2016-06-30 22:54:34 -0700

    Add styling for Result component.

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,25 +1,22 @@
 import React from 'react';
+import styles from './Result.css';
 
 export default (props) => {
   const { height, weight } = props;
   const BMI = (weight / ( height * height )) * 703;
 
   function verdict() {
-    if (BMI < 18.5) {
-      return 'You are underweight.';
+    if (BMI < 18.5 || BMI > 30.0) {
+      return 'Please seek medical attention immediately.';
     } else if (BMI > 18.5 && BMI < 24.9) {
       return 'You are of normal weight.';
     } else if (BMI > 25.0 && BMI < 29.9) {
       return 'You are overweight.';
-    } else if (BMI > 30.0) {
-      return 'Please seek medical attention immediately.';
     }
     return 'Submit your height and weight info.';
   }
 
-  return (<div>
-    <p>Height: {height === '' ? '0' : height}</p>
-    <p>Weight: {weight === '' ? '0' : weight}</p>
+  return (<div className={`${styles.comp}`}>
     <h4>BMI: {isNaN(BMI) ? '0' : BMI.toFixed(1)}</h4>
     <h4>{verdict()}</h4>
   </div>);
